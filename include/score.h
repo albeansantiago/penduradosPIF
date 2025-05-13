b589e4a76f73537aac8bd21d42cb9d3ce4ab88ab
albeansantiago <ascb@cesar.school>
2025-05-13 18:12:18 -0300

    Update score.h

--- include/score.h
+++ include/score.h
@@ -1,1 +1,24 @@
+/**
+ * score.h
+ * Created on Nov, 18th 2024
+ * Author: Fernando Cyreno, Marcello Fernandes e Ivo Caetano
+ * Based on Imperative and Functional Programming from CEZAR School"
+*/
 
+#ifndef __SCORE_H__
+#define __SCORE_H__
+
+#include <stdio.h>
+
+// Estrutura para armazenar o nome e a pontuação do jogador
+typedef struct {
+    char nome[9];
+    int ponto;
+} JogadorPonto;
+
+// Declaração das funções
+int salvarJogador(JogadorPonto jogador);
+JogadorPonto *lerJogadores();
+int quantidadeJogadores();
+
+#endif /* __SCORE_H__ */
