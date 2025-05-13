9e648ae10b2df41dd6440fe05222fc7c37b8a394
albeansantiago <ascb@cesar.school>
2025-05-13 17:23:19 -0300

    organização

--- src/game.c
+++ src/game.c
@@ -5,97 +5,98 @@
 #include <timer.h>
 #include <screen.h>
 #include <keyboard.h>
-#include <util.h>
 #include <score.h>
 #include <game.h>
 
-void forca(int erro)
-    if(erro == 0)
-        printf("\n-----------------")
-        printf("\n|               |")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n__")
-    else if(erro == 1)
-        printf("\n-----------------")
-        printf("\n|               |")
-        printf("\n|               O")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n__")
-     else if(erro == 2)
-        printf("\n-----------------")
-        printf("\n|               |")
-        printf("\n|               O")
-        printf("\n|               |")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n__")
-    else if(erro == 3)
-        printf("\n-----------------")
-        printf("\n|               |")
-        printf("\n|               O")
-        printf("\n|              -|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n__")
-    else if(erro == 4)
-        printf("\n-----------------")
-        printf("\n|               |")
-        printf("\n|               O")
-        printf("\n|              -|-")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n__")
-     else if(erro == 5)
-        printf("\n-----------------")
-        printf("\n|               |")
-        printf("\n|               O")
-        printf("\n|              -|-")
-        printf("\n|              /")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n__")
-    else if(erro == 6)
-        printf("\n-----------------")
-        printf("\n|               |")
-        printf("\n|               O")
-        printf("\n|              -|-")
-        printf("\n|              / \\")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n|")
-        printf("\n__")
-    
+void forca(int erro) {
+    if(erro == 0) {
+        printf("\n-----------------");
+        printf("\n|               |");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n__");
+    } else if(erro == 1) {
+        printf("\n-----------------");
+        printf("\n|               |");
+        printf("\n|               O");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n__");
+    } else if(erro == 2) {
+        printf("\n-----------------");
+        printf("\n|               |");
+        printf("\n|               O");
+        printf("\n|               |");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n__");
+    } else if(erro == 3) {
+        printf("\n-----------------");
+        printf("\n|               |");
+        printf("\n|               O");
+        printf("\n|              -|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n__");
+    } else if(erro == 4) {
+        printf("\n-----------------");
+        printf("\n|               |");
+        printf("\n|               O");
+        printf("\n|              -|-");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n__");
+    } else if(erro == 5) {
+        printf("\n-----------------");
+        printf("\n|               |");
+        printf("\n|               O");
+        printf("\n|              -|-");
+        printf("\n|              /");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n__");
+    } else if(erro == 6) {
+        printf("\n-----------------");
+        printf("\n|               |");
+        printf("\n|               O");
+        printf("\n|              -|-");
+        printf("\n|              / \\");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|");
+        printf("\n|       PERDEU O JOGO");
+        printf("\n__");
+    }
+}
 int main(void) {
-    forca()
+    forca(2);
+
     return 0;
 }
